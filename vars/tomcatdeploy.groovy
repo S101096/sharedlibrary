e4c9453d171fd4e9dd77acb8a5bdcc0d9d4f9d8b
S101096 <73982029+S101096@users.noreply.github.com>
2024-05-14 12:55:17 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,6 +1,6 @@
-def call()
+def call(tomcatIP)
 sshagent(['tomcat9jenkins']) {
-     sh "scp -o StrictHostKeyChecking=no target/mavenjenkins.war ec2-user@172.31.36.0:/opt/tomcat9/webapps"
-     sh "ssh ec2-user@172.31.36.0 /opt/tomcat9/bin/shutdown.sh"
-     sh "ssh ec2-user@172.31.36.0 /opt/tomcat9/bin/startup.sh"
+     sh "scp -o StrictHostKeyChecking=no target/mavenjenkins.war ec2-user@${tomcatIP}:/opt/tomcat9/webapps"
+     sh "ssh ec2-user@${tomcatIP} /opt/tomcat9/bin/shutdown.sh"
+     sh "ssh ec2-user@${tomcatIP} /opt/tomcat9/bin/startup.sh"
 }
